16145867b6dd0ae0e68982f93810c632e5b8eaf0
Bryce Gibson <bryce@gibson-consulting.com.au>
2023-03-04 07:23:53 +1100

    Fix height with a more trips.

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,6 +1,6 @@
 export default function Wrapper({ children }) {
   return (
-    <div className='flex h-full w-full items-center justify-center flex-col max-sm:p-3'>
+    <div className='flex min-h-full w-full items-center justify-center flex-col max-sm:p-3'>
       <div className='text-left max-w-md'>{children}</div>
     </div>
   )
