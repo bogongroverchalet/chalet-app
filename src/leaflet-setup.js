ab4ee8b92cafbeb5b569846d11788950dd10ac48
Bryce Gibson <bryce@gibson-consulting.com.au>
2023-03-09 22:05:26 +1100

    Fix the default icon config.

--- src/leaflet-setup.js
+++ src/leaflet-setup.js
@@ -4,10 +4,8 @@ import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import leaflet from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
-const DefaultIcon = leaflet.divIcon({
+leaflet.Icon.Default = leaflet.DivIcon.extend({
   html: renderToString(<FontAwesomeIcon icon={faLocationDot} className='text-3xl text-black-700' />),
   iconSize: [20, 20],
   iconAnchor: [10, 33],
 })
-
-leaflet.Icon.Default = leaflet.Marker.prototype.options.icon = DefaultIcon
