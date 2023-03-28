13146b04a8b0c4a22caa3e8cf2955dc68c2d99b8
Bryce Gibson <bryce@gibson-consulting.com.au>
2023-03-28 20:49:48 +1100

    Add some extra links.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Wrapper from './Wrapper'
 import { Link } from 'react-router-dom'
 import Tooltip from '@mui/material/Tooltip'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import LaunchIcon from '@mui/icons-material/Launch'
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
 
 export default function App() {
@@ -18,17 +19,17 @@ export default function App() {
         <br />
       </p>
       <ul className='text-2xl'>
-        <li className='mb-2'>
+        <li className='mb-4'>
           <Link to='trips' className='flex items-center'>
             Trips ideas <ChevronRightIcon className='ml-1' fontSize='large' />
           </Link>
         </li>
-        <li>
+        <li className='mb-4'>
           <Link
             to='https://www.youtube.com/playlist?list=PLPY3isagL_7H0x_XfVkldG_iSRP8Qo2oB'
             className='flex items-center'
           >
-            Participant info videos{' '}
+            Participant info videos
             <Tooltip
               title={
                 <div>
@@ -40,9 +41,21 @@ export default function App() {
               arrow
               classes={{ tooltip: 'text-center !text-[1rem]' }}
             >
-              <InfoOutlinedIcon className='ml-3' />
-            </Tooltip>{' '}
-            <ChevronRightIcon className='ml-1' fontSize='large' />
+              <InfoOutlinedIcon className='ml-2' />
+            </Tooltip>
+            <LaunchIcon className='ml-2' />
+          </Link>
+        </li>
+        <li className='mb-4'>
+          <Link to='http://www.bom.gov.au/vic/forecasts/fallscreek.shtml' className='flex items-center'>
+            Falls Creek weather
+            <LaunchIcon className='ml-2' />
+          </Link>
+        </li>
+        <li className='mb-4'>
+          <Link to='http://www.bom.gov.au/vic/warnings/' className='flex items-center'>
+            Weather alerts
+            <LaunchIcon className='ml-2' />
           </Link>
         </li>
       </ul>
